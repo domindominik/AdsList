56c0700314559fd07a7c472d9c3772f7316cb958
dominik-lenovo <dominwro@gmail.com>
2019-10-19 11:17:13 +0200

    Initial commit.

---
+++ src/main/java/main/AdsListApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsListApplication.class, args);
+	}
+
+}
